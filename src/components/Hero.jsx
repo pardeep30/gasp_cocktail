80a95a1089a8c998d51f4cf7e51369e2021aef25
Pardeep <sidhupardeep30@gmail.com>
2025-06-26 14:02:23 +0530

    implement cocktails section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,12 +67,12 @@ const Hero = () => {
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title"> MOJITO</h1>
+        <h1 className="title z-50 "> PRANAVEE</h1>
 
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
-          className="left-leaf"
+          className="left-leaf -z-50"
         />
 
         <img
